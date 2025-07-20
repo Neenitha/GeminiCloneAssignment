0885aa181da12f9be18a892de78e7176f5166b8e
Neenitha Gopalan <neenitha@32crayons.com>
2025-07-20 11:57:20 +0530

    Added env NODE_CACHE_TTL

--- backend/src/middlewares/checkCache.js
+++ backend/src/middlewares/checkCache.js
@@ -1,7 +1,8 @@
 const nodeCache = require('node-cache');
 const httpStatus = require('http-status');
+const config = require('../config/config');
 
-const cache = new nodeCache({ stdTTL: 60, checkperiod: 120 });
+const cache = new nodeCache({ stdTTL: config.NODE_CACHE_TTL, checkperiod: 120 });
 
 async function checkCache( req, res, next ) {
   const key = res.locals.userId;
