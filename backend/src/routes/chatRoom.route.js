bfdd518709a72b144f815278c31859abbf8f6f2e
Neenitha Gopalan <neenitha@32crayons.com>
2025-07-20 11:45:04 +0530

    Added checkCache in chatRoomUserGet API

--- backend/src/routes/chatRoom.route.js
+++ backend/src/routes/chatRoom.route.js
@@ -4,12 +4,14 @@ const express = require('express');
 const authMiddleware = require('../middlewares/checkAuth');
 const router = express.Router();
 const chatRoomController = require('../controllers/chatRoom.controller');
+const cacheMiddleware = require('../middlewares/checkCache');
 
 router.use(authMiddleware.jwtAuthenticate);
 
 router.post('/', chatRoomController.chatRoomPOST);
-router.get('/', chatRoomController.chatRoomUSERGET);
 router.get('/:id', chatRoomController.chatRoomGET);
 router.post('/:id/message', chatRoomController.chatRoomSENDMESSAGE);
+
+router.get('/', cacheMiddleware.checkCache, chatRoomController.chatRoomUSERGET);
 
 module.exports = router;
